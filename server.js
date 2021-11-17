6c0443ae805b85051e6d18a95e818bc67acdaed1
Thiago <tbbecchi@escuelasproa.edu.ar>
2021-11-17 13:00:54 -0300

    primer commit

--- server.js
+++ server.js
@@ -1,32 +1,36 @@
-const express = require ("express");
-const app = express();
-const path = require("path")
-const port = 3000;
+    const express = require ("express");
+    const app = express();
+    const path = require("path")
+    const port = 2500;
+
+    app.listen(port, () => console.log('listo'));
+    app.use(express.json());
+    app.use( express.static( "public" ) );
+    app.set('view engine', 'ejs');
+    
+    app.set('views', path.join(__dirname, '/views'));
+    app.set('public',path.join(__dirname, '/public'));
 
 
-app.listen(port, () => console.log('Example app listening on port 4000!'));
-
-app.use(express.json());
-app.use( express.static( "public" ) );
-
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '/views'));
-app.set('public',path.join(__dirname, '/public'));
-
-
-let productos = {
-    "naranja": {"name":"Naranja",'precio':5,"tipo":"fruta","stock":true,"img": '/img/naranj.jpg'},
-    "lechuga": {"name":"Lechuga",'precio': 12,"tipo":"verdura","stock": true,"img": '/img/lechu.jpg'},
-    "manzana": {"name":"Manzana",'precio':3,"tipo":"fruta","stock":true,"img": '/img/manza.jpg'},
-    "pera": {"name":"Pera",'precio':15,"tipo":"fruta","stock":false,"img": '/img/pera.jpg'},
-    "sandia": {"name":"Sandia",'precio': 40,"tipo":"fruta","stock":false,"img": '/img/sandia.jpg'},
-    "zanahoria":{"name":"Zanahoria",'precio':16,"tipo":"verdura","stock":true,"img": '/img/zana.jpg'},
-    "zapallo":{"name":"Zapallo",'precio':25,"tipo":"verdura","stock":true,"img": '/img/zapal.jpg'},
-    "espinaca":{"name":"Espinaca",'precio':30,"tipo":"verdura","stock":false,"img": '/img/espin.jpg'},
-    "frutilla":{"name":"Frutilla",'precio':20,"tipo":"frutilla","stock":true,"img": '/img/fruti.jpg'},
-}
-
-app.get('/verdu',function (req,res){
-    let name = " La verdu ejs ";
-    res.render('prueba.ejs',{productos:productos});
-});
+let peliculas =  {
+    
+        "inception":{"titulo":"inception","director":"nolan",'duracion':10,"genero":"drama","disponivilidad":false,"img": '/img/inception.png'},
+        "tenet":{"titulo":"tenet","director":"nolan",'duracion':10,"genero":"drama","disponivilidad":false,"img": '/img/tenet.jpg'},
+        "taxi driver":{"titulo":"taxi driver","director":"scorsese",'duracion':10,"genero":"drama","disponivilidad":false,"img": '/img/taxi driver.jfif'},
+        "goodfellas":{"titulo":"goodfellas","director":"scorsese",'duracion':10,"genero":"drama","disponivilidad":false,"img": '/img/goodfellas.jfif'},
+        "your name?":{"titulo":"your name?","director":"nolan",'duracion':10,"genero":"drama","disponivilidad":false,"img": '/img/your name.jpg'},
+        "no respires":{"titulo":"no respires","director":"nolan",'duracion':10,"genero":"drama","disponivilidad":true,"img": '/img/no respires.jfif'},
+        "jurasic park":{"titulo":"jurassic park","director":"nolan",'duracion':10,"genero":"drama","disponivilidad":false,"img": '/img/jurassic park.jpg'},
+        "iron man":{"titulo":"iron man","director":"nolan",'duracion':10,"genero":"drama","disponivilidad":false,"img": '/img/iron man.jpg'},
+        "capitan america":{"titulo":"capitan america","director":"nolan",'duracion':10,"genero":"drama","disponivilidad":false,"img": '/img/capitan america.jpg'},
+        "star wars":{"titulo":"star wars","director":"nolan",'duracion':10,"genero":"drama","disponivilidad":false,"img": '/img/star wars.jpg'},
+    }
+app.get('/videoclub',function (req,res){
+    res.render('prueba.ejs',{peliculas:peliculas });
+}) ;
+app.get('/disponible',function (req,res){
+    res.render('disponible.ejs',{peliculas:peliculas });
+}) ;
+app.get('/terror',function (req,res){
+    res.render('terror.ejs',{peliculas:peliculas });
+}) ;
